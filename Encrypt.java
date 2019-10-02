3dedc69f65f1053080d971bd5c3e430aaaa90b8a
Benjamin Grynblat <benjamin.grynblat@gmail.com>
2019-10-02 14:56:01 +0100

    Use 60s rolling password

--- Encrypt.java
+++ Encrypt.java
@@ -8,145 +8,104 @@ import java.math.BigInteger;
 import java.nio.ByteOrder;
 import javax.xml.bind.DatatypeConverter;
 import java.nio.charset.StandardCharsets;
+import java.nio.charset.Charset;
 
 public class Encrypt {
 
-// private static final char[] LOOKUP_TABLE_LOWER = new char[]{0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66};
-// private static final char[] LOOKUP_TABLE_UPPER = new char[]{0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46};
+  public static String encrypt(String plainText, String keyBase64, String ivBase64) throws Exception
+  {
+      byte[] plainTextArray = plainText.getBytes(StandardCharsets.UTF_8);
+      byte[] keyArray = DatatypeConverter.parseBase64Binary(keyBase64);
+      byte[] iv = DatatypeConverter.parseBase64Binary(ivBase64);
 
-// public static String encodeHex(byte[] byteArray, boolean upperCase, ByteOrder byteOrder) {
-
-//     // our output size will be exactly 2x byte-array length
-//     final char[] buffer = new char[byteArray.length * 2];
-
-//     // choose lower or uppercase lookup table
-//     final char[] lookup = upperCase ? LOOKUP_TABLE_UPPER : LOOKUP_TABLE_LOWER;
-
-//     int index;
-//     for (int i = 0; i < byteArray.length; i++) {
-//         // for little endian we count from last to first
-//         index = (byteOrder == ByteOrder.BIG_ENDIAN) ? i : byteArray.length - i - 1;
-
-//         // extract the upper 4 bit and look up char (0-A)
-//         buffer[i << 1] = lookup[(byteArray[index] >> 4) & 0xF];
-//         // extract the lower 4 bit and look up char (0-A)
-//         buffer[(i << 1) + 1] = lookup[(byteArray[index] & 0xF)];
-//     }
-//     return new String(buffer);
-// }
-
-// public static String encodeHex(byte[] byteArray) {
-//     return encodeHex(byteArray, true, ByteOrder.BIG_ENDIAN);
-// }
-
-// public static SecretKey generateKey(String password) 
-//     throws NoSuchAlgorithmException, InvalidKeySpecException 
-// { 
-//     return new SecretKeySpec(password.getBytes(), "AES"); 
-// }
-
-// public static byte[] encryptMsg(String message, SecretKey secret)
-//     throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidParameterSpecException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException 
-// { 
-//    /* Encrypt the message. */
-//    Cipher cipher = null; 
-//    cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-//    cipher.init(Cipher.ENCRYPT_MODE, secret); 
-
-//    System.out.println("IV: "+new String(cipher.getIV()));
-
-//    byte[] cipherText = cipher.doFinal(message.getBytes("UTF-8")); 
-//    return cipherText; 
-// }
-
-// public static String decryptMsg(byte[] cipherText, SecretKey secret) 
-//     throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidParameterSpecException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, UnsupportedEncodingException 
-// {
-//     /* Decrypt the message, given derived encContentValues and initialization vector. */
-//     Cipher cipher = null;
-//     cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-//     cipher.init(Cipher.DECRYPT_MODE, secret); 
-//     String decryptString = new String(cipher.doFinal(cipherText), "UTF-8");
-//     return decryptString; 
-// }
-
-    public static String encrypt(String plainText, String keyBase64, String ivBase64) throws Exception
-    {
-        byte[] plainTextArray = plainText.getBytes(StandardCharsets.UTF_8);
-        byte[] keyArray = DatatypeConverter.parseBase64Binary(keyBase64);
-        byte[] iv = DatatypeConverter.parseBase64Binary(ivBase64);
-
-        SecretKeySpec secretKey = new SecretKeySpec(keyArray, "AES");
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");   
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey, new IvParameterSpec(iv));
-        return new String(DatatypeConverter.printBase64Binary(cipher.doFinal(plainTextArray)));
-    }
-
-    public static String decrypt(String messageBase64, String keyBase64, String ivBase64) throws Exception {
-
-        byte[] messageArray = DatatypeConverter.parseBase64Binary(messageBase64);
-        byte[] keyArray = DatatypeConverter.parseBase64Binary(keyBase64);
-        byte[] iv = DatatypeConverter.parseBase64Binary(ivBase64);
-
-        SecretKey secretKey = new SecretKeySpec(keyArray, "AES");
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
-        cipher.init(Cipher.DECRYPT_MODE, secretKey, new IvParameterSpec(iv));
-        return new String(cipher.doFinal(messageArray));
-    }
-
-    public static void main(String[] args) {
-
-      try
-      {   
-          long time = System.currentTimeMillis();
-          String salt = "salt&pepper";
-          String password = (args.length > 0 ? args[0] : "#ABCDEF#ms&%6hcp");
-          String toencrypt = "RTV@"+salt+"_"+time+"_"+salt;
-
-          String password_b64 = Base64.getEncoder().encodeToString(password.getBytes());
-
-          // String ivBase64 = "AcynMwikMkW4c7+mHtwtfw==";
-          String ivBase64 = "Acyn/wikMkW4c7+mHtwtfq==";
-
-          String cipherText = encrypt(toencrypt, password_b64, ivBase64);
-          // String decryptedCipherText = decrypt(cipherText, encryptionKeyBase64, ivBase64);
-
-          System.out.println("Plaintext: " + toencrypt);
-          System.out.println("Password: " + password+" | base64: "+password_b64);
-          System.out.println("Ciphertext: " + cipherText);
-          // System.out.println("Decrypted text: " + decryptedCipherText);
-      }
-      catch (Exception e)
-      {
-          System.out.println(e.toString());
-      }
-    }
+      SecretKeySpec secretKey = new SecretKeySpec(keyArray, "AES");
+      Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");   
+      cipher.init(Cipher.ENCRYPT_MODE, secretKey, new IvParameterSpec(iv));
+      return new String(DatatypeConverter.printBase64Binary(cipher.doFinal(plainTextArray)));
   }
 
-  // public static void main(String args[]) {
+  public static String decrypt(String messageBase64, String keyBase64, String ivBase64) throws Exception {
 
-  //   try {
-  //     
+      byte[] messageArray = DatatypeConverter.parseBase64Binary(messageBase64);
+      byte[] keyArray = DatatypeConverter.parseBase64Binary(keyBase64);
+      byte[] iv = DatatypeConverter.parseBase64Binary(ivBase64);
 
-  //     System.out.println("TO ENCRYPT: "+toencrypt);
-  //     // System.out.println("PASSWORD: "+password);
-  //     // SecretKey key = generateKey(password);
-  //     // String b64key = Base64.getEncoder().encodeToString(key.getEncoded());
-  //     // System.out.println("SECRET KEY: "+b64key);
+      SecretKey secretKey = new SecretKeySpec(keyArray, "AES");
+      Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+      cipher.init(Cipher.DECRYPT_MODE, secretKey, new IvParameterSpec(iv));
+      return new String(cipher.doFinal(messageArray));
+  }
 
-  //     // byte[] enc = encryptMsg(toencrypt, key);
-  //     // System.out.println("ENCRYPTED: "+new String(enc));
-      
-  //     // String hex = encodeHex(enc);
-  //     // System.out.println("ENCRYPTED HEX: "+hex);
+  public static String generateToken(String salt, String salt2) {
+      try
+      {
+          long time = System.currentTimeMillis();
 
-  //     // String enc_string = Base64.getEncoder().encodeToString(enc);
-  //     // System.out.println("ENCRYPTED base64: "+enc_string);
+          long time_60s = time/60000;
+          time_60s *= 60000;
 
-  //   } catch(Exception e) {
-  //     System.out.println("ERROR:"+e);
-  //   }
+          String hash = md5(time_60s+"-5T9[SLt{H.@EtBm$");
+          String password = hash.substring(1, 17);
+          String password_b64 = Base64.getEncoder().encodeToString(password.getBytes());
 
-  // }
+          System.err.println(password+" -> "+password_b64+" ("+time+")");
 
-// }
+          String ivBase64 = "Acyn/wikMkW4c7+mHtwtfq==";
+
+          String cipherText = encrypt("RTV@"+salt+"-"+time+"-"+salt2, password_b64, ivBase64);
+          return cipherText;
+      } catch (Exception e) {
+          System.out.println(e.toString());
+      }
+      return null;
+  }
+
+  public static String md5(String s)
+    {
+        MessageDigest digest;
+        try
+        {
+            digest = MessageDigest.getInstance("MD5");
+            digest.update(s.getBytes(Charset.forName("US-ASCII")),0,s.length());
+            byte[] magnitude = digest.digest();
+            BigInteger bi = new BigInteger(1, magnitude);
+            String hash = String.format("%0" + (magnitude.length << 1) + "x", bi);
+            return hash;
+        } catch (NoSuchAlgorithmException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+  public static void main(String[] args) {
+
+    try
+    {   
+        // long time = System.currentTimeMillis();
+        // String salt = "salt&pepper";
+        // String password = (args.length > 0 ? args[0] : "#ABCDEF#ms&%6hcp");
+        // String toencrypt = "RTV@"+salt+"_"+time+"_"+salt;
+
+        // String password_b64 = Base64.getEncoder().encodeToString(password.getBytes());
+
+
+        // String cipherText = encrypt(toencrypt, password_b64, ivBase64);
+        // // String decryptedCipherText = decrypt(cipherText, encryptionKeyBase64, ivBase64);
+
+        // System.out.println("Plaintext: " + toencrypt);
+        // System.out.println("Password: " + password+" | base64: "+password_b64);
+        // System.out.println("Ciphertext: " + cipherText);
+        // // System.out.println("Decrypted text: " + decryptedCipherText);
+      String mEmail = "Andrew.donald";
+      String APIKEY = "fc2760ce-22f0-4f1a-a18b-7b889d47781e";
+      String token = generateToken(md5("salt_"+mEmail), md5(mEmail+"_pepper_"+APIKEY));
+
+      System.out.println(token);
+
+    }
+    catch (Exception e)
+    {
+        System.out.println(e.toString());
+    }
+  }
+}
--- decrypt.js
+++ decrypt.js
@@ -36,9 +36,13 @@ function decrypt(textb64, pass_b64, iv_b64){
     return dec;
 }
 
+const t = parseInt(Date.now()/60000)*60000
+var password = t+"-5T9[SLt{H.@EtBm$"
+password = crypto.createHash('md5').update(password).digest('hex').substring(1,17)
 
-const password = "#ABCDEF#ms&%6hcp"
 const pass_b64 = Buffer.from(password, 'utf8').toString('base64');
+
+console.log(password, "->", pass_b64, `(${t})`)
 
 // var keyBase64 = "DWIzFkO22qfVMgx2fIsxOXnwz10pRuZfFJBvf4RS3eY=";
 var keyBase64 = pass_b64;
@@ -50,6 +54,31 @@ var cipherText = process.argv[2] || encrypt(plainText, keyBase64, ivBase64);
 var decryptedCipherText = decrypt(cipherText, keyBase64, ivBase64);
 
 console.log('Algorithm: ' + getAlgorithm(keyBase64));
-console.log('Plaintext: ' + plainText);
+!process.argv[2] && console.log('Plaintext: ' + plainText);
 console.log('Ciphertext: ' + cipherText);
 console.log('Decoded Ciphertext: ' + decryptedCipherText);
+
+const part = decryptedCipherText.split("@")[1]
+const split = part.split("-")
+const h1 = split[0]
+const time = split[1]
+const h2 = split[2]
+
+const user_email = "Andrew.donald"
+const APIKEY = "fc2760ce-22f0-4f1a-a18b-7b889d47781e"
+const hashes = {
+    h1: `salt_${user_email}`,
+    h2: `${user_email}_pepper_${APIKEY}`
+}
+var r1 = crypto.createHash('md5').update(hashes.h1).digest('hex');
+var r2 = crypto.createHash('md5').update(hashes.h2).digest('hex');
+// console.log(hash)
+
+const str = `RTV@${r1}-${time}-${r2}`
+
+console.log("time:", new Date(parseInt(time)))
+console.log("DEC:",decryptedCipherText)
+console.log("RES:",str)
+
+if(str === decryptedCipherText)     console.log("SUCCESS")
+else                                console.log("INVALID")
